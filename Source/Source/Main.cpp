f53eda3fa050126d5495a23b408b0b8693a89f36
Rico Tyrell <rico@redringrico.com>
2016-05-08 03:25:02 +0100

    Added a usage print function for an incorrect command line configuration

--- Source/Source/Main.cpp
+++ Source/Source/Main.cpp
@@ -3,7 +3,10 @@
 #include <GameListServer.hpp>
 #include <GameServer.hpp>
 #include <Time.hpp>
+#include <vector>
 #include <unistd.h>
+
+void PrintUsage( );
 
 // Move this later
 int KeyPress( )
@@ -34,10 +37,20 @@ int main( int p_Argc, char **p_ppArgv )
 {
 	std::cout << "[TERMINAL] Server" << std::endl;
 	std::cout << "Version: " << GIT_BUILD_VERSION << std::endl;
+	std::vector< TERMINAL::NetServer * > Servers;
+
+	if( p_Argc == 1 )
+	{
+		PrintUsage( );
+
+		return 0;
+	}
+
 	std::cout << "Configured for the following services:" << std::endl;
 	std::cout << "\tAuthentication" << std::endl;
 	std::cout << "\tLobby" << std::endl;
 	std::cout << "\tGame" << std::endl;
+
 
 	TERMINAL::GameListServer ListServer;
 	TERMINAL::GameServer GameServer;
@@ -59,5 +72,26 @@ int main( int p_Argc, char **p_ppArgv )
 	ListServer.Terminate( );
 
 	return 0;
+}
+
+void PrintUsage( )
+{
+	std::cout << std::endl << std::endl;
+
+	std::cout << "Hello and welcome to this server-side program to launch one "
+		"of many services." << std::endl << std::endl;
+	
+	std::cout << "It would appear that you have made an error in your "
+		"selection; fear not, for I " << std::endl << "will help you to "
+		"correct your mistake!" << std::endl << std::endl;
+	
+	std::cout << "In order to successfully deploy this program, you will need "
+		"to select one of " << std::endl << "two services with the "
+		"following:" << std::endl;
+	std::cout << "Flag | Description" << std::endl;
+	std::cout << "-g     Starts a game server" << std::endl;
+	std::cout << "-l     Starts a game list server" << std::endl << std::endl;
+
+	std::cout << "That is all.  Good day to you." << std::endl << std::endl;
 }
 
